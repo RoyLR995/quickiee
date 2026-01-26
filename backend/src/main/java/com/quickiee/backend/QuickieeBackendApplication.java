ff5b195e5fcfd69648204dfb5684049d0de725d3
RoyLR995 <roylikhitha2003@gmail.com>
2026-01-26 11:59:26 +0530

    Bootstrap Spring Boot backend without DB

---
+++ backend/src/main/java/com/quickiee/backend/QuickieeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.quickiee.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickieeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickieeBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/quickiee/backend/QuickieeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickiee.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickieeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
