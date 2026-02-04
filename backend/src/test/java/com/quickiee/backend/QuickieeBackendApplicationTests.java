d3e6c719a9f1988bf70ad3b8bfef67f18695e7f0
RoyLR995 <roylikhitha2003@gmail.com>
2026-02-04 17:32:06 +0530

    Triggering CI run#6

--- backend/src/test/java/com/quickiee/backend/QuickieeBackendApplicationTests.java
+++ backend/src/test/java/com/quickiee/backend/QuickieeBackendApplicationTests.java
@@ -1,9 +1,12 @@
 package com.quickiee.backend;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import io.qameta.allure.junit5.AllureJunit5;
 
 @SpringBootTest
+@ExtendWith(AllureJunit5.class)
 class QuickieeBackendApplicationTests {
 
 	@Test
--- backend/src/test/java/com/quickiee/backend/testsupport/ApiTestBase.java
+++ backend/src/test/java/com/quickiee/backend/testsupport/ApiTestBase.java
@@ -1,9 +1,11 @@
 package com.quickiee.backend.testsupport;
 
 import io.restassured.RestAssured;
+import io.qameta.allure.junit5.AllureJunit5;
 import io.qameta.allure.restassured.AllureRestAssured;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
@@ -11,6 +13,7 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(AllureJunit5.class)
 public abstract class ApiTestBase {
 
     @LocalServerPort
